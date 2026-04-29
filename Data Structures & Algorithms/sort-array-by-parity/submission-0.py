d2e44d1a4df9329f38889ca5b0a0296fae57868d
Varshini207 <168453378+Varshini207@users.noreply.github.com>
2026-04-29 23:00:42 +0530

    Add: sort-array-by-parity - submission-0

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        o=[]
+        e=[]
+        for a in nums:
+            if a%2==0:
+                e.append(a)
+            else:
+                o.append(a)
+        return e+o
