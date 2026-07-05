ca8e8dd8ebd502796530d81dd9270580f89efb86
Varshini207 <168453378+Varshini207@users.noreply.github.com>
2026-07-05 23:42:08 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        d={}
+        for i,v in enumerate(nums):
+            c=target-v
+            if c in d:
+                return[d[c],i]
+            d[v]=i
+        return []
+        
+
